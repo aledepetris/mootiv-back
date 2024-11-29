7fea25455a722d43905faf644d4b0065304e140c
alejandro.depetris <alejandro.depetris@neoris.com>
2024-11-29 13:17:19 -0300

    First commit - initialize springboot project

---
+++ src/main/java/com/mootiv/mootiv/MootivApplication.java
@@ -0,0 +1,13 @@
+package com.mootiv.mootiv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MootivApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MootivApplication.class, args);
+	}
+
+}
