47a671231bf63ffa047f87cc5255118deba6d963
aledepetris <aledepetris@outlook.com>
2024-12-06 14:54:21 -0300

    definition of endpoints for cycles, weeks and day resource

--- src/main/java/com/mootiv/controller/student/cycle/TrainingCycleController.java
+++ src/main/java/com/mootiv/controller/student/cycle/TrainingCycleController.java
@@ -1,4 +1,12 @@
 package com.mootiv.controller.student.cycle;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bff/v1/student/{idStudent}/cycle")
 public class TrainingCycleController {
+
+
+
 }
--- src/main/java/com/mootiv/controller/student/cycle/TrainingDayController.java
+++ src/main/java/com/mootiv/controller/student/cycle/TrainingDayController.java
@@ -1,4 +1,9 @@
 package com.mootiv.controller.student.cycle;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bff/v1/student/{idStudent}/cycle/{idCycle}/week/{idWeek}/day")
 public class TrainingDayController {
 }
